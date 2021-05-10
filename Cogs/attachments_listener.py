cb29b0c7b4efc22454ce3dd1a9fdd700b022d3c7
TheKing-OfTime <68960526+TheKing-OfTime@users.noreply.github.com>
2021-05-11 01:53:25 +0300

    Refactoring

--- Cogs/attachments_listener.py
+++ Cogs/attachments_listener.py
@@ -24,89 +24,80 @@ class AntiCrashAttachments(commands.Cog):
             json.dump(self.cache, write_f)
 
     @commands.Cog.listener()
-    @commands.bot_has_permissions(manage_messages=True)
     async def on_message(self, message: discord.Message):
-        lst = message.attachments
-        crash_detected = False
-        if message.content in self.cache["black_listed_links"]:
-            crash_detected = True
-            self.statistic["head"]["auto_crash_content"] += 1
-            self.statistic["data"].append({"type": "black_listed_links",
-                                           "message_id": message.id,
-                                           "detected_content": message.content,
-                                           "is_crush_auto": True,
-                                           "is_crush": None
-                                           })
-        if len(lst) != 0 and crash_detected == False:
-            try:
-                reaction = True
-                await message.add_reaction(loading_emoji())
-            except:
-                reaction = None
-        else:
-            reaction = None
 
-        for attachment in lst:
-            if attachment.url in self.cache["black_listed_links"]:
-                crash_detected = True
-                self.statistic["head"]["auto_crash_content"] += 1
-                self.statistic["data"].append({"type": "black_listed_links",
-                                               "message_id": message.id,
-                                               "detected_content": message.content,
-                                               "attachment_id": attachment.id,
-                                               "attachment_url": attachment.url,
-                                               "is_crush_auto": True,
-                                               "is_crush": None
-                                               })
+        is_detected = False
+
+        for link in self.cache["black_listed_links"]:
+            if link in message.content:
+                is_detected = True
+                self.statistic["data"].append({
+                    "type": "by_bl_link_in_message",
+                    "message_id": message.id,
+                    "is_crush_auto": True,
+                    "is_crush": None
+                })
                 break
+
+        if not is_detected and message.attachments != []:
+
+            if self.statistic["head"]["visualisation_mode"] == 2:
+                await message.add_reaction(loading_emoji())
+
+            for attachment in message.attachments:
+                if attachment.content_type.startswith('video') or attachment.content_type == "image/gif":
+                    if attachment.url in self.cache["black_listed_links"]:
+                        is_detected = True
+                        self.statistic["data"].append({
+                            "type": "by_bl_link_in_attachment",
+                            "message_id": message.id,
+                            "attachment_id": attachment.id,
+                            "attachment_url": attachment.url,
+                            "is_crush_auto": True,
+                            "is_crush": None
+                        })
+                        break
+                    else:
+                        attachment_content = str(await attachment.read())
+                        empty_bytes = attachment_content.count("00")
+                        all_bytes = attachment.size
+                        probability = round(empty_bytes/all_bytes*1000, 3)
+
+                        #print(empty_bytes, all_bytes, probability)
+
+                        if probability >= int(self.statistic["head"]["detect_ratio"]):
+                            is_detected = True
+                            self.statistic["data"].append({
+                                "type": "by_empty_bytes",
+                                "message_id": message.id,
+                                "attachment_id": attachment.id,
+                                "attachment_url": attachment.url,
+                                "empty_bytes": empty_bytes,
+                                "all_bytes": all_bytes,
+                                "probability": probability,
+                                "detect_ratio": self.statistic["head"]["detect_ratio"],
+                                "is_crush_auto": True,
+                                "is_crush": None
+                            })
+
+                            self.cache["black_listed_links"].append(attachment.url)
+
+                            break
+
+        if is_detected:
+            if self.statistic["head"]["active_mode"]:
+                await message.delete()
             else:
-                self.statistic["head"]["counter"] = len(self.statistic["data"])
-                attachment_bytes = await attachment.read()
+                if self.statistic["head"]["visualisation_mode"] == 2:
+                    await message.remove_reaction(loading_emoji(), message.guild.me)
+                    await message.add_reaction(disagree_emoji())
 
-                dead_bytes = str(attachment_bytes).count("00")
-                all_bytes = len(str(attachment_bytes))
-                probability_of_crash_content = round(dead_bytes / all_bytes * 1000, 2)
-
-                # print(dead_bytes, all_bytes, f"\nВероятность краш контента {probability_of_crash_content}%")
-
-                if probability_of_crash_content > self.statistic["head"]["detect_ratio"]:
-                    self.statistic["head"]["auto_crash_content"] += 1
-                self.statistic["data"].append({"type": "probability",
-                                               "message_id": message.id,
-                                               "attachment_id": attachment.id,
-                                               "attachment_url": attachment.url,
-                                               "dead_bytes": dead_bytes,
-                                               "all_bytes": all_bytes,
-                                               "probability": probability_of_crash_content,
-                                               "is_crush_auto": True if probability_of_crash_content >
-                                                                        self.statistic["head"][
-                                                                            "detect_ratio"] else False,
-                                               "is_crush": None
-                                               })
-                if probability_of_crash_content > self.statistic["head"]["detect_ratio"]:
-                    self.cache["black_listed_links"].append(attachment.url)
-        self.statistic["head"]["counter"] = len(self.statistic["data"])
-
-        with open("Database/Statistic.json", "w") as write_f:
-            json.dump(self.statistic, write_f)
-        with open("Database/cache.json", "w") as write_f:
-            json.dump(self.cache, write_f)
-
-        if reaction is not None:
-            await message.remove_reaction(loading_emoji(), message.guild.me)
-        if crash_detected:
-            probability_of_crash_content = self.statistic["head"]["detect_ratio"] + 1
-        if probability_of_crash_content < self.statistic["head"]["detect_ratio"]:
-            if reaction is not None:
+        else:
+            if self.statistic["head"]["visualisation_mode"] == 2 and message.attachments != []:
+                await message.remove_reaction(loading_emoji(), message.guild.me)
                 await message.add_reaction(agree_emoji())
                 await asyncio.sleep(3)
                 await message.remove_reaction(agree_emoji(), message.guild.me)
-        else:
-            if self.statistic["head"]["active_mode"]:
-                await message.delete()
-                await message.channel.send(f"Заблокирован опасный контент от пользователя: {message.author.mention}", delete_after=5)
-            else:
-                await message.add_reaction(disagree_emoji())
 
     @commands.command()
     async def get_stat(self, ctx: commands.Context, _type="default", numb=0):
