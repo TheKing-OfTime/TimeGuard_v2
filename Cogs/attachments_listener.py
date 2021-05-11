996a27c451888b2fb918adc685b44be4f6ca003c
TheKing-OfTime <68960526+TheKing-OfTime@users.noreply.github.com>
2021-05-11 15:19:26 +0300

    Settings update

--- Cogs/attachments_listener.py
+++ Cogs/attachments_listener.py
@@ -85,12 +85,24 @@ class AntiCrashAttachments(commands.Cog):
                             break
 
         if is_detected:
+            self.statistic["head"]["counter"] = len(self.statistic["data"])
+            self.statistic["head"]["auto_crash_content"] += 1
+
             if self.statistic["head"]["active_mode"]:
                 await message.delete()
+                await message.channel.send(f"Удалён опасный контент от {message.author.mention}", delete_after=5)
             else:
                 if self.statistic["head"]["visualisation_mode"] == 2:
                     await message.remove_reaction(loading_emoji(), message.guild.me)
                     await message.add_reaction(disagree_emoji())
+                elif self.statistic["head"]["visualisation_mode"] == 1:
+                    await message.add_reaction(disagree_emoji())
+
+            with open("Database/Statistic.json", "w") as write_f:
+                json.dump(self.statistic, write_f)
+
+            with open("Database/cache.json", "w") as write_f:
+                json.dump(self.cache, write_f)
 
         else:
             if self.statistic["head"]["visualisation_mode"] == 2 and message.attachments != []:
@@ -100,23 +112,27 @@ class AntiCrashAttachments(commands.Cog):
                 await message.remove_reaction(agree_emoji(), message.guild.me)
 
     @commands.command()
-    async def get_stat(self, ctx: commands.Context, _type="default", numb=0):
+    async def get_stat(self, ctx: commands.Context, _type="default", numb=-1):
         _type_aliases_v1 = ["detail", "d", "more", "m"]
         embed = discord.Embed(colour=discord.Colour.blurple())
         if _type == "default":
             embed.title = "Сводка"
+
             embed.add_field(name="Проанализированных файлов:", value=self.statistic["head"]["counter"], inline=False)
-            embed.add_field(name="Подозрительных из них:", value=self.statistic["head"]["auto_crash_content"],
-                            inline=False)
+            embed.add_field(name="Подозрительных из них:", value=self.statistic["head"]["auto_crash_content"],inline=False)
             embed.add_field(name="Активный режим:", value=self.statistic["head"]["active_mode"], inline=False)
+            embed.add_field(name="Режим отображения прогресса:", value=self.statistic["head"]["visualisation_mode"], inline=False)
             embed.add_field(name="Порог детекта:", value=self.statistic["head"]["detect_ratio"], inline=False)
+
         elif _type in _type_aliases_v1:
             embed.title = "Инфо по случаю"
+
             if not int(numb) + 1 > len(self.statistic["data"]):
                 for item in self.statistic["data"][numb].items():
                     embed.add_field(name=item[0], value=item[1], inline=False)
             else:
                 raise commands.BadArgument
+
         await ctx.send(embed=embed)
 
     @commands.command(aliases=["s"])
@@ -127,19 +143,43 @@ class AntiCrashAttachments(commands.Cog):
                 if value == 'on':
                     self.statistic["head"]["active_mode"] = True
                     await self.bot.change_presence(
-                        activity=discord.Activity(type=discord.ActivityType.playing, name='Фильрую опасный контент'))
+                        activity=discord.Activity(type=discord.ActivityType.playing, name='Филтьрую опасный контент'))
                     await ctx.message.add_reaction(agree_emoji())
+
                 elif value == 'off':
                     self.statistic["head"]["active_mode"] = False
                     await self.bot.change_presence(
                         activity=discord.Activity(type=discord.ActivityType.playing, name="Анализирую данные"))
                     await ctx.message.add_reaction(agree_emoji())
+                    
                 else:
                     raise commands.BadArgument
 
             elif option in ["detect_ratio", "ratio", 'r']:
                 self.statistic["head"]["detect_ratio"] = int(value)
                 await ctx.message.add_reaction(agree_emoji())
+
+            elif option in ["visualisation_mode", "visualisation", "v"]:
+                if int(value) >= 0 and int(value) <= 2:
+                    self.statistic["head"]["visualisation_mode"] = int(value)
+                else:
+                    raise commands.BadArgument
+
+            elif option in ["cache_add", "ca"]:
+                self.cache["black_listed_links"].append(value)
+
+            elif option in ["cache_add", "ca"]:
+                try:
+                    self.cache["black_listed_links"].remove(value)
+                except ValueError:
+                    ctx.send(f"Значение {value} не найдено в кеше")
+
+            with open("Database/Statistic.json", "w") as write_f:
+                json.dump(self.statistic, write_f)
+
+            with open("Database/cache.json", "w") as write_f:
+                json.dump(self.cache, write_f)
+
         except Exception as error:
             await ctx.send(f"Ошибка: {error}")
 
